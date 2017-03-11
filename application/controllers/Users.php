61e8070440b3058b7054c9c6bc3c9e5d0049b67d
awalin yudhana <otoy.destroyed@gmail.com>
2017-03-11 14:03:42 +0700

    agent

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -15,9 +15,9 @@ class Users extends REST_Controller
         parent::__construct();
     }
 
-    public function by_id_get()
+    public function by_id_get($id)
     {
-        $user = $this->db->get_where('users', ['user_id' => 1])->row_array();
+        $user = $this->db->get_where('users', ['user_id' => $id])->row_array();
         $user['profile_path'] = site_url('uploads/'. $user['profile']);
 
         $this->set_response($user, REST_Controller::HTTP_OK);
